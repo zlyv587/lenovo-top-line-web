0fc4a394a060d6d77b9f14450b72c9bfe126ef0c
CedarLiu <lxs19951201@163.com>
2018-01-26 11:17:59 +0800

    首页

---
+++ src/view/home-page/index.vue
@@ -0,0 +1,309 @@
+<template>
+	<div class="home">
+		<div class="topline container">
+			<h2><span>图标</span>早间头条</h2>
+			<div class="videos">
+				<div class="item big">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="rightitem">
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+
+						</div>
+					</div>
+				</div>
+				<div class="bottomitem">
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="message container">
+			<h2><span>图标</span>资讯</h2>
+			<div class="videos">
+				<div class="item big">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="rightitem">
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+
+						</div>
+					</div>
+				</div>
+				<div class="ad">
+					广告
+				</div>
+			</div>
+		</div>
+		<div class="amuse container">
+			<h2><span>图标</span>娱乐</h2>
+			<div class="videos">
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="joke container">
+			<h2><span>图标</span>搞笑</h2>
+			<div class="videos">
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="item">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="talkshow container">
+			<h2><span>图标</span>脱口秀</h2>
+			<div class="videos">
+				<div class="item big">
+					<img class="img" src="222.png" alt="...">
+					<div class="msg">
+					</div>
+				</div>
+				<div class="rightitem">
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+						</div>
+					</div>
+					<div class="item">
+						<img class="img" src="222.png" alt="...">
+						<div class="msg">
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="loadmore">展开了解更多</div>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="less" src="./style.less"></style>
+<script src="./script.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/view/home-page/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Lzhang on 2017/12/11.
+ */
+
+export default {
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+       // // this.home.changeCount();
+       //  this.changeCount()
+    },
+    methods: {
+
+    }
+}
